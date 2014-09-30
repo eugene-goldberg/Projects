88e58e825db3a3a53f1e3e6ef37f827cb4a7a1e2
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-09-30 13:41:55 -0400

    Database added

--- ngBookLibrary/ngBookLibrary.Server/Controllers/BookController.cs
+++ ngBookLibrary/ngBookLibrary.Server/Controllers/BookController.cs
@@ -10,30 +10,37 @@ namespace ngBookLibrary.Server.Controllers
 {
     public class BookController : ApiController
     {
-        List<Book> books = new List<Book>
+        //List<Book> books = new List<Book>
+        //{
+        //    new Book {Id = 1, Title = "Test Book", Author = "Prasad Honrao", Price = 1234, IsAvailable = true},
+        //    new Book {Id = 2, Title = "Programming Fundamentals", Author = "Scott Hanselman", Price = 700, IsAvailable = true},
+        //    new Book {Id = 3, Title = "Building Web Apps using AngularJS", Author = "Dan Wahlin", Price = 700, IsAvailable = false},
+        //    new Book {Id = 4, Title = "ASP.Net All In One", Author = "Scott Guthrie", Price = 9999, IsAvailable = true},
+        //    new Book {Id = 5, Title = "Let Us C", Author = "Yashwant Kanetkar", Price = 500, IsAvailable = false},
+        //    new Book {Id = 6, Title = "Mastering SQL", Author = "Rob Dan", Price = 1500, IsAvailable = true},
+        //    new Book {Id = 7, Title = "C# Fundamentals", Author = "Scott Allen", Price = 1000, IsAvailable = true},
+        //    new Book {Id = 8, Title = "Advanced C#", Author = "Scott Allen", Price = 1500, IsAvailable = false},
+        //    new Book {Id = 9, Title = "UML A to Z", Author = "Don Box", Price = 2500, IsAvailable = false},
+        //    new Book {Id = 10, Title = "JavaScript from scratch", Author = "Prasad honrao", Price = 999, IsAvailable = true}
+        //};
+
+        ngBookLibraryDbEntities db; 
+
+        public BookController()
         {
-            new Book {Id = 1, Title = "Test Book", Author = "Prasad Honrao", Price = 1234, IsAvailable = true},
-            new Book {Id = 2, Title = "Programming Fundamentals", Author = "Scott Hanselman", Price = 700, IsAvailable = true},
-            new Book {Id = 3, Title = "Building Web Apps using AngularJS", Author = "Dan Wahlin", Price = 700, IsAvailable = false},
-            new Book {Id = 4, Title = "ASP.Net All In One", Author = "Scott Guthrie", Price = 9999, IsAvailable = true},
-            new Book {Id = 5, Title = "Let Us C", Author = "Yashwant Kanetkar", Price = 500, IsAvailable = false},
-            new Book {Id = 6, Title = "Mastering SQL", Author = "Rob Dan", Price = 1500, IsAvailable = true},
-            new Book {Id = 7, Title = "C# Fundamentals", Author = "Scott Allen", Price = 1000, IsAvailable = true},
-            new Book {Id = 8, Title = "Advanced C#", Author = "Scott Allen", Price = 1500, IsAvailable = false},
-            new Book {Id = 9, Title = "UML A to Z", Author = "Don Box", Price = 2500, IsAvailable = false},
-            new Book {Id = 10, Title = "JavaScript from scratch", Author = "Prasad honrao", Price = 999, IsAvailable = true}
-        };
+            db = new ngBookLibraryDbEntities();
+        }
 
         // GET: api/Book
         public IEnumerable<Book> Get()
         {
-            return books;
+            return db.Books;
         }
 
         // GET: api/Book/5
         public Book Get(int id)
         {
-            return books.Where(b => b.Id == id).Single();
+            return db.Books.Where(b => b.Id == id).Single();
         }
 
         // POST: api/Book
